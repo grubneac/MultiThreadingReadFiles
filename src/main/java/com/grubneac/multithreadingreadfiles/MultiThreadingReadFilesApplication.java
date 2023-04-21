a7653fb11468bb41638bf9f921077192b8213fc6
grubneac <grubneac@yandex.ru>
2023-04-21 14:30:01 +0300

    Initial commit

---
+++ src/main/java/com/grubneac/multithreadingreadfiles/MultiThreadingReadFilesApplication.java
@@ -0,0 +1,13 @@
+package com.grubneac.multithreadingreadfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadingReadFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiThreadingReadFilesApplication.class, args);
+    }
+
+}
